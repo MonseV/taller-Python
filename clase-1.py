7ad79e2476ff13bfde0d98199b0f249088e06a65
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2022-06-27 13:45:48 -0500

    Hola Mundo
    Conceptos básicos, Hola Mundo y variables

---
+++ clase-1.py
@@ -0,0 +1,15 @@
+from xmlrpc.client import Boolean
+
+
+print("Hola Mundo")
+
+
+# Enteros -> 1,2,3,4,5,6,7,8,9
+# Flotantes -> 3.14, 2.5, 1.1
+
+# Entero           Flotante(Decimal)
+# 1 es diferente a 1.0
+
+# Cadenas -> 'Hola mundo'
+
+# Booleano -> verdadero y falso
