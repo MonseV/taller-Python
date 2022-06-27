1cf4ba31aa8450b3b325d481173b520aadd5fa1c
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2022-06-27 17:30:23 -0500

    Programación Estructurada
    variables, tipos de datos, operadores, estructura secuencial y condicional

---
+++ clase-2.py
@@ -0,0 +1,67 @@
+#Variables y Tipos de datos
+
+#identificador      asignacion          valor
+edad                    =                25 # Una variable con un dato de tipo entero
+
+#Cadenas
+mi_nombre = "Monserrat Vásquez"
+fecha_nacimiento = "1992/10/12"
+
+#Booleanos    Veradero(true)    False(falso)
+""" mayor_edad = True """
+mayor_edad = False
+
+#Flotantes
+pi = 3.1416
+
+""" print (edad)
+print(mi_nombre)
+print(mayor_edad)
+print(pi) """
+#El tema de la memoria, se encarga Python
+
+
+#Operadores
+#Operaciones aritmeticas
+suma = 2 + 2
+resta = 2- 2
+division = 10 / 5
+division_entera = 18 // 5 # Division con resultado de numero entero
+multiplicacion = 10 * 5
+
+""" print(suma)
+print(resta)
+print(division)
+print(division_entera)
+print(multiplicacion) """
+
+
+#Estructura condicional
+#Operadores de comparacion
+
+#if condicion (se cumple):
+  #ejecutara el código que está aquí
+  #si no
+    #no se muestra
+
+""" if mayor_edad:
+  print("Mayoría de edad alcanzada.") """
+
+#if 4 > 3:
+#  print("4 es mayor que 3")
+
+#Operadores Lógicos
+#and
+
+mayor_edad = False
+dinero = True
+
+""" if mayor_edad and dinero:
+  print("Le vendo el alcohol")
+ """
+#or
+""" if mayor_edad or dinero:
+  print("le vendo alcohol") """
+
+#not
+not True #devulve lo contrario
